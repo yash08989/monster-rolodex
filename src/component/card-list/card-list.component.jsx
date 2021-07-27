1c69f3b51f595ea20dd303fe287c336a55c60771
yash08989 <singhyashovardhan10@gmail.com>
2021-07-27 21:09:40 +0530

    adding files for gh-pages

---
+++ src/component/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './card-list.styles.css'
+import { Card } from '../card/card.component';
+
+export const CardList = props => (
+   
+    <div className='card-list'> 
+         {
+            props.monsters.map(monsters => (
+              <Card key={monsters.id} monster = {monsters} />
+            ) )
+          }  
+    </div>
+);
